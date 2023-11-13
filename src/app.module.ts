b544c8c9ea37b28bdc8572c4aa87839abb6b31ac
camiloruiz <camiloruizvidal@gmail.com>
2023-11-13 12:08:00 -0500

    Added global enviroment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { TestModule } from './modules/test/test.module';
 
 @Module({
-  imports: [TestModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TestModule],
   controllers: [AppController],
   providers: [AppService],
 })

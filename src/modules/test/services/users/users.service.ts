30bc5d64d36eac6524f8d25fbe560fb51a8d29fa
camiloruiz <camiloruizvidal@gmail.com>
2023-11-13 19:44:30 -0500

    Send data rabbitMQ

--- src/modules/test/services/users/users.service.ts
+++ src/modules/test/services/users/users.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { IUser, IUserResponse } from '../../interfaces/IUser.interface';
 import { UsersApiResponse } from './dto/UsersApiResponse';
+import * as amqp from 'amqplib';
+
+
+
 
 @Injectable()
 export class UsersService {
 
-  private readonly BASE_URL: string = process.env.BASE_URL;
+  private readonly BASE_URL: string;
+  private readonly AMQP_URL: string;
 
-  constructor() {}
+  constructor() {
+    this.BASE_URL = process.env.BASE_URL
+    this.AMQP_URL = `amqp://${process.env.AMQP_HOST}:${process.env.AMQP_PORT}`;
+  }
 
   public async getUsers(): Promise<IUser[]> {
     const response = await axios.get<IUserResponse[]>(`${this.BASE_URL}/users`);
@@ -22,6 +30,29 @@ export class UsersService {
 
   public async sendPairUsers(users: IUser[]) {
     const usersPair: IUser[] = users.filter((user: IUser) => user.id % 2 == 0);
+    this.sendDataRabbitMQ(usersPair);
+  }
+
+  private async sendDataRabbitMQ(usersPair: IUser[]) {
+    try {
+      const connection = await amqp.connect(this.AMQP_URL);
+      const channel = await connection.createChannel();
+
+      await channel.assertExchange('users', 'fanout', { durable: false });
+      await channel.assertQueue('users-requested', { durable: false });
+      await channel.bindQueue('users-requested', 'users', '');
+
+      channel.sendToQueue('users-requested', Buffer.from(JSON.stringify(usersPair)));
+
+      setTimeout(() => {
+        connection.close();
+      }, 500);
+
+      console.log({success: true})
+    } catch(error) {
+      console.log({error})
+    }
+
   }
 
 

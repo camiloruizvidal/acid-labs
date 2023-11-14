99ee6a2b01a5e4d5f1a73f6db179e2d0da5acd7e
camiloruiz <camiloruizvidal@gmail.com>
2023-11-13 21:25:19 -0500

    Deleted bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
-import { TestModule } from './modules/test/test.module';
+import { TestModule } from './modules/Test/test.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), TestModule],
